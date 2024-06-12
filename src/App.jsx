48f4d227b54bc9f505725ee0b55b3244f6cc6382
JuaneDev <121892094+juaneprograming@users.noreply.github.com>
2024-06-12 16:20:46 +0000

    imagenes y nombres de pokemones traidos deesde el fetching de datos

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,54 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState(null)
+  const [pokemones, setPokemones] = useState([])
 
   //nos va a permitir hacer la peticion http
   useEffect(() =>{
-    fetch ("")
+    const getPokemones = async () => {
+
+      //recuperamos el listado de los pokemones
+
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+    const listaPokemones = await response.json()
+    const { results } = listaPokemones
+
+        const newPokemones = results.map( async (pokemon) => {
+          //  const poke = {}
+
+           const response = await fetch(pokemon.url)
+           const poke = await response.json()
+
+          return {
+            id: poke.id,
+            name: poke.name,
+            img: poke.sprites.other.dream_world.front_default
+          }
+
+        })
+        setPokemones(await Promise.all(newPokemones))
+    }
+
+    getPokemones ()
   }, [])
 
   return (
     <>
       <div className='App'>
-        <h1>Fetch like a Pro</h1>
-        <div className='card'></div>
+        <h1>Pokedex</h1>
+
+        {
+          pokemones.map(pokemon => {
+            return (
+              <div>
+                <img src={pokemon.img} alt={pokemon.name} />
+                <p>{pokemon.name}</p>
+                <span>{pokemon.id}</span>
+              </div>
+            )
+          })
+        }
+
       </div>
     </>
   )
